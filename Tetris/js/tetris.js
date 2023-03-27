16a5aaa0e24f2ac28e6ad04d6f27cb909c78548c
ljyj115 <kimyeji4176@gmail.com>
2023-03-27 22:14:23 +0900

    Update tetris.js

--- Tetris/js/tetris.js
+++ Tetris/js/tetris.js
@@ -1,16 +1,48 @@
+//DOM
 const playground = document.querySelector(".playground > ul");
 
-console.log(playground)
+//Setting
+const GAME_ROWS = 20;
+const GAME_COLS = 10;
 
-for(let i = 0; i < 20; i++) {
+//varibles
+let score = 0;
+let duration = 500;
+let downInterval;
+let tempMovingItem;
+
+const BLOCKS ={
+    
+}
+
+const movingItem = {
+    type: "",
+    direction: 0,
+    top: 0,
+    left: 0,
+};
+
+init()
+
+//functions
+function init() {
+    for(let i = 0; i < GAME_ROWS; i++) {
+        prependNewLine()
+    }
+}
+
+
+
+
+function prependNewLine() {
     const li = document.createElement("li");
     const ul = document.createElement("ul");
-    for(let j = 0; j < 10; j++) {
+    for(let j = 0; j < GAME_COLS; j++) {
         const matrix = document.createElement("li");
         ul.prepend(matrix);
 
     }
     li.prepend(ul)
     playground.prepend(li)
-
 }
+function prependNewLine()
