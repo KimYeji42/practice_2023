5d874c4781aaf6222883b565975f1c8a3ebecb9d
KimYeji42 <125680485+KimYeji42@users.noreply.github.com>
2023-04-05 10:58:05 +0900

    Update DicApp.java

--- Week04/src/DicApp.java
+++ Week04/src/DicApp.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 class Dictionary {
     Scanner in = new Scanner(System.in);
-    String[] ko = {"cat" , "dog", "rabbit", "bear", "pig"};
-    String[] en = {"고양이", "강아지", "토끼", "곰", "돼지"};
+    public String[] ko = {"고양이", "강아지", "토끼", "곰", "돼지"};
+    public String[] en = {"cat" , "dog", "rabbit", "bear", "pig"};
 
-    String Word(String word) {
+    public String Word(String word) {
         for(int i=0; i<ko.length; i++) {
             if(word.equals(ko[i])) {
                 return en[i];
@@ -16,33 +16,25 @@ class Dictionary {
 
     public void Run() {
         while(true) {
-            System.out.print("한글 단어?");
-            words = sc.next();
-            if(words.equals("그만")) {
+            System.out.print("검색할 한글단어를 입력하세요. (종료할려면 '종료' 입력)  : ");
+            String word = in.next();
+            if(word.equals("종료")) {
                 break;
             }
-            String result = Dictionary.kor2Eng(word);
+            String result = Word(word);
             if(result==(null))
-                System.out.println(words+"은(는) 저의 사전에 없습니다.");
+                System.out.println(word+"은(는) 없는 단어.");
             else
-                System.out.println(words+"은(는) "+result);
+                System.out.println(word+"은(는) "+result);
         }
     }
-
-    public void Search () {
-        System.out.print("검색할 한글 단어를 입력하세요. : ");
-        String SearchWord = in.next();
-        if(int i=0; i<ko.length; i++)
-        {
-        }
-    }
-
-
 
 }
 
 public class DicApp {
     public static void main(String[] args) {
-
+    	Dictionary dictionary = new Dictionary();
+		dictionary.Run();
+        
     }
 }
